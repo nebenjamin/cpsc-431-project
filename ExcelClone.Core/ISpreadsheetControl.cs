408b5b30dd8ce847f5300f8c4e5a5218318155c3
EmoryCook <EmoryCook@f4101352-c044-0410-813c-79886a7bac79>
2008-02-11 21:22:19 +0000

    Modified the SpreadsheetControl to add functions to support the parser.  Added LoadSheet function.

--- ExcelClone.Core/ISpreadsheetControl.cs
+++ ExcelClone.Core/ISpreadsheetControl.cs
@@ -4,12 +4,14 @@ using System.Text;
 
 namespace ExcelClone.Core
 {
-  interface ISpreadsheetControl
-  {
-    void InvalidateCell(CellKey key);
-    void InvalidateCell(int r, int c);
-    void UpdateCellValue(CellKey key, string value);
-    void UpdateCellFormula(CellKey key, string formula);
-    void ClearCell(CellKey key);
-  }
+    interface ISpreadsheetControl
+    {
+        void InvalidateCell(CellKey key);
+        void InvalidateCell(int r, int c);
+        void UpdateCellValue(CellKey key, string value);
+        void UpdateCellFormula(CellKey key, string formula);
+        void ClearCell(CellKey key);
+        void LoadSheet(ICellCollection c);
+        void CellChanged(CellKey key);
+    }
 }
--- ExcelClone.Core/SpreadsheetControl.cs
+++ ExcelClone.Core/SpreadsheetControl.cs
@@ -5,39 +5,62 @@ using System.Collections;
 
 namespace ExcelClone.Core
 {
-  class SpreadsheetControl : ISpreadsheetControl
-  {    
-    public SpreadsheetControl()
+    class SpreadsheetControl : ISpreadsheetControl
     {
+        private SpreadsheetModel m;
+        private Queue<CellKey> InvalidCells;
+
+        public SpreadsheetControl()
+        {
+            m = new SpreadsheetModel(new CellCollection());
+            InvalidCells = new Queue<CellKey>();
+        }
+
+        #region ISpreadsheetControl Members
+
+        public void InvalidateCell(CellKey key)
+        {
+            InvalidCells.Enqueue(key);
+        }
+
+        public void CellChanged(CellKey key)
+        {
+            InvalidateCell(key);
+            while (InvalidCells.Count > 0)
+            {
+                //updateCellValue(key, parse(key, value)); call the parse function for this cell key
+            }
+        }
+
+        public void InvalidateCell(int r, int c)
+        {
+            this.InvalidateCell(new CellKey(r, c));
+        }
+
+        public void UpdateCellValue(CellKey key, string value)
+        {
+            m.Cells[key].Value = value;
+        }
+
+        public void UpdateCellFormula(CellKey key, string formula)
+        {
+            m.Cells[key].Formula = formula;
+            //repaint
+        }
+
+        public void ClearCell(CellKey key)
+        {
+            m.Cells[key].Formula = null;
+            m.Cells[key].Value = "";
+            //repaint
+        }
+
+        public void LoadSheet(ICellCollection c)
+        {
+            m.Cells = (CellCollection)c;
+            //change  view!           
+        }
+
+        #endregion
     }
-
-    #region ISpreadsheetControl Members
-
-    public void InvalidateCell(CellKey key)
-    {
-      //call the parse function for this cell key
-    }
-
-    public void InvalidateCell(int r, int c)
-    {
-      this.InvalidateCell(new CellKey(r, c));
-    }    
-
-    public void UpdateCellValue(CellKey key, string value)
-    {
-      throw new Exception("The method or operation is not implemented.");
-    }
-
-    public void UpdateCellFormula(CellKey key, string formula)
-    {
-      throw new Exception("The method or operation is not implemented.");
-    }
-
-    public void ClearCell(CellKey key)
-    {
-      throw new Exception("The method or operation is not implemented.");
-    }
-
-    #endregion
-  }
 }
