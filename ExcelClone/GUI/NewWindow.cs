8acf5dcc9565898010362a477bf5d0f46ddccce4
long.mai.tamu <long.mai.tamu@f4101352-c044-0410-813c-79886a7bac79>
2008-03-03 16:45:16 +0000

    Created a new window. Making changes to the old window keeps giving design errors. The new window consists of menu, toolbar, and contextmenu. The spreedsheet needs to be added to it.

---
+++ ExcelClone/GUI/NewWindow.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExcelClone.Gui
+{
+    public partial class NewWindow : Form
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
