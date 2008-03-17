99cf46f5acb33320101b485eadb7a431fb79b8f3
long.mai.tamu <long.mai.tamu@f4101352-c044-0410-813c-79886a7bac79>
2008-03-17 20:06:57 +0000

    Added SpreadsheetUserControl. Spreadsheet functionalities need to be readded.

--- trunk/ExcelClone/GUI/SpreadsheetUserControl.cs
+++ trunk/ExcelClone/GUI/SpreadsheetUserControl.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Text;
 using System.Windows.Forms;
 
 namespace ExcelClone.Gui
 {
-    public partial class NewWindow : Form
+    public partial class SpreadsheetUserControl : UserControl
     {
-        public NewWindow()
+        public SpreadsheetUserControl()
         {
             InitializeComponent();
         }
