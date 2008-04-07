def22e1d91da17a73c1f5ca305ca4ff6d85a0e8d
n.benjamin07 <n.benjamin07@f4101352-c044-0410-813c-79886a7bac79>
2008-04-07 20:02:47 +0000

    Added Open/Save to Controller.cs

--- ExcelClone/Controller/Controller.cs
+++ ExcelClone/Controller/Controller.cs
@@ -6,102 +6,105 @@ using ExcelClone.Functions;
 
 namespace ExcelClone
 {
-    class Controller
+  class Controller
+  {
+    private Controller()
     {
-        private Controller()
-        {
-            SpreadsheetModel = new SpreadsheetModel(new CellCollection());
-            Parser = new Parser();
-        }
-
-        private SpreadsheetModel spreadsheetModel;
-        public SpreadsheetModel SpreadsheetModel
-        {
-            get { return spreadsheetModel; }
-            set { spreadsheetModel = value; }
-        }
-
-        private Parser parser;
-        public Parser Parser
-        {
-            get { return parser; }
-            set { parser = value; }
-        }
-
-        public void ExecuteCommand(object sender, EventArgs e, CommandType command)
-        {
-            switch (command)
-            {
-                case CommandType.Exit:
-                    ExecuteExit();
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public void ExecuteNew()
-        {
-        }
-        public void ExecuteOpen()
-        {
-          DataIO.DataIO opener = new ExcelClone.DataIO.DataIO();
-          opener.AddSpreadsheet(spreadsheetModel);
-          spreadsheetModel = opener.LoadBook();       
-        }
-        public void ExecuteClose()
-        {
-        }
-        public void ExecuteSave()
-        {
-          DataIO.DataIO saver = new ExcelClone.DataIO.DataIO();
-          saver.AddSpreadsheet(spreadsheetModel);
-          saver.SaveBook();
-        }
-        public void ExecuteSaveAs()
-        {
-        }
-        public void ExecuteCut()
-        {
-        }
-        public void ExecuteCopy()
-        {
-        }
-        public void ExecutePaste()
-        {
-        }
-        public void ExecuteChart()
-        {
-        }
-        public void ExecuteInsertWorksheet()
-        {
-        }
-        public void ExecuteInsertFunction(object sender, EventArgs e)
-        {
-        }
-        public void ExecuteExit()
-        {
-            System.Windows.Forms.Application.Exit();
-        }
-
-        public static Controller Instance
-        {
-            get
-            {
-                return ControllerCreator.ControllerInstance;
-            }
-
-        }
-        private sealed class ControllerCreator
-        {
-            private static readonly Controller _instance = new Controller();
-            public static Controller ControllerInstance
-            {
-                get
-                {
-                    return _instance;
-                }
-            }
-        }
+      SpreadsheetModel = new SpreadsheetModel(new CellCollection());
+      Parser = new Parser();
     }
+
+    private SpreadsheetModel spreadsheetModel;
+    public SpreadsheetModel SpreadsheetModel
+    {
+      get { return spreadsheetModel; }
+      set { spreadsheetModel = value; }
+    }
+
+    private Parser parser;
+    public Parser Parser
+    {
+      get { return parser; }
+      set { parser = value; }
+    }
+
+    public void ExecuteCommand(object sender, EventArgs e, CommandType command)
+    {
+      switch (command)
+      {
+        case CommandType.Exit:
+          ExecuteExit();
+          break;
+        case CommandType.Open:
+          ExecuteOpen();
+          break;
+        default:
+          break;
+      }
+    }
+
+    public void ExecuteNew()
+    {
+    }
+    public void ExecuteOpen()
+    {
+      DataIO.DataIO opener = new ExcelClone.DataIO.DataIO();
+      opener.AddSpreadsheet(spreadsheetModel);
+      spreadsheetModel = opener.LoadBook();
+    }
+    public void ExecuteClose()
+    {
+    }
+    public void ExecuteSave()
+    {
+      DataIO.DataIO saver = new ExcelClone.DataIO.DataIO();
+      saver.AddSpreadsheet(spreadsheetModel);
+      saver.SaveBook();
+    }
+    public void ExecuteSaveAs()
+    {
+    }
+    public void ExecuteCut()
+    {
+    }
+    public void ExecuteCopy()
+    {
+    }
+    public void ExecutePaste()
+    {
+    }
+    public void ExecuteChart()
+    {
+    }
+    public void ExecuteInsertWorksheet()
+    {
+    }
+    public void ExecuteInsertFunction(object sender, EventArgs e)
+    {
+    }
+    public void ExecuteExit()
+    {
+      System.Windows.Forms.Application.Exit();
+    }
+
+    public static Controller Instance
+    {
+      get
+      {
+        return ControllerCreator.ControllerInstance;
+      }
+
+    }
+    private sealed class ControllerCreator
+    {
+      private static readonly Controller _instance = new Controller();
+      public static Controller ControllerInstance
+      {
+        get
+        {
+          return _instance;
+        }
+      }
+    }
+  }
 }
