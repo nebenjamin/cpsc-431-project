5cde37c60faeb58e55bb2f7b29cd827fd6e95e4c
TxAggieMichael <TxAggieMichael@f4101352-c044-0410-813c-79886a7bac79>
2008-04-07 18:48:04 +0000

    Checked in CommandType.cs

---
+++ ExcelClone/Controller/CommandType.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelClone
+{
+    enum CommandType
+    {
+        New,
+        Open,
+        Close,
+        Save,
+        SaveAs,
+        Copy,
+        Cut,
+        Paste,
+        Exit,
+        InsertWorksheet,
+        InsertGraph,
+        insertFunction,
+        About
+    }
+}
