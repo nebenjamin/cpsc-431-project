d3d81564a30e03fa243b1cbeb5f580d9cd973f8b
android8 <android8@f4101352-c044-0410-813c-79886a7bac79>
2008-05-01 04:11:23 +0000

    Cleaning up the graphConfig code.

--- ExcelClone/Graphs/graphConfig.cs
+++ ExcelClone/Graphs/graphConfig.cs
@@ -31,105 +31,92 @@ namespace ExcelClone.Graphs
         // will put on settings that were chosen before on the graph
         private void InitConfig()
         {
-
+            // Sets graph title if checkDrawTitle is checked.
 
             if (gr.draw_title)
             {
-                textBox1.Text = gr.TitleString;
-                checkBox1.Checked = true;
+                GraphTitleBox.Text = gr.TitleString;
+                checkDrawTitle.Checked = true;
             }
 
-            checkBox2.Checked = gr.LegendOn;
+            checkDrawLegend.Checked = gr.LegendOn;
 
-
-
-
-
-            listBox2.DataSource = gr.LegendColors;
-
-
-            listBox1.DataSource = gr.LegendLabels;
+            listBoxGraphColors.DataSource = gr.LegendColors;
+            listBoxGraphSeries.DataSource = gr.LegendLabels;
 
         }
 
-        private void setConfigTabAccess()
+
+        private void GraphTitleBox_TextChanged(object sender, EventArgs e)
         {
-            if (gr.LegendColors.Count < gr.LegendLabels.Count)
+            gr.TitleString = GraphTitleBox.Text;
+        }
+
+        private void checkDrawGraphTitle_CheckedChanged(object sender, EventArgs e)
+        {
+            gr.draw_title = checkDrawTitle.Checked;
+        }
+
+        private void checkDrawLegend_CheckedChanged(object sender, EventArgs e)
+        {
+            gr.LegendOn = checkDrawLegend.Checked;
+        }
+
+        private void buttonRenameSeries_Click(object sender, EventArgs e)
+        {
+
+            gr.LegendLabels[listBoxGraphSeries.SelectedIndex] = textBoxRename.Text;
+
+            if (listBoxGraphSeries.SelectedIndex < gr.LegendLabels.Count-1)
+                listBoxGraphSeries.SelectedIndex++;
+
+            else if (listBoxGraphSeries.SelectedIndex == gr.LegendLabels.Count-1)
+                listBoxGraphSeries.SelectedIndex = 0;
+
+            listBoxGraphSeries.DataSource = null;
+            listBoxGraphSeries.DataSource = gr.LegendLabels;
+
+            textBoxRename.Text = gr.LegendLabels[listBoxGraphSeries.SelectedIndex];
+        }
+
+        private void buttonRemoveColor_Click(object sender, EventArgs e)
+        {
+
+            // Check to ensure LegendColors does not underrun LegendLabels
+            if((listBoxGraphColors.SelectedItem != null) & (gr.LegendColors.Count > gr.LegendLabels.Count))
             {
-                tabControl1.SelectedIndex = 0;
-            }
-        }
+                //Removes the selected color from LegendColors, updates listBoxGraphColors
+                gr.LegendColors.RemoveAt(listBoxGraphColors.SelectedIndex);
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            gr.TitleString = textBox1.Text;
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            gr.draw_title = checkBox1.Checked;
-        }
-
-        private void textBox1_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "";
-            checkBox1.Checked = true;
-        }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            gr.LegendOn = checkBox2.Checked;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-            gr.LegendLabels[listBox1.SelectedIndex] = textBox2.Text;
-
-            if (listBox1.SelectedIndex < gr.LegendLabels.Count-1)
-                listBox1.SelectedIndex++;
-
-            else if (listBox1.SelectedIndex == gr.LegendLabels.Count-1)
-                listBox1.SelectedIndex = 0;
-
-            listBox1.DataSource = null;
-            listBox1.DataSource = gr.LegendLabels;
-
-            textBox2.Text = gr.LegendLabels[listBox1.SelectedIndex];
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if((listBox2.SelectedItem != null) & (gr.LegendColors.Count > gr.LegendLabels.Count))
-            {
-                gr.LegendColors.RemoveAt(listBox2.SelectedIndex);
-
-                listBox2.DataSource = null;
-                listBox2.DataSource = gr.LegendColors;
+                listBoxGraphColors.DataSource = null;
+                listBoxGraphColors.DataSource = gr.LegendColors;
              }
-
-
             
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void buttonAddColor_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedItem != null)
+            if (listBoxGraphSeries.SelectedItem != null)
             {
-
+                //Opens Add Color Dialog, Adds Result, Updates listBoxGraphColors.
                 colorDialog1.ShowDialog();
                 gr.LegendColors.Add(colorDialog1.Color);
 
-                listBox2.DataSource = null;
-                listBox2.DataSource = gr.LegendColors;
-
+                listBoxGraphColors.DataSource = null;
+                listBoxGraphColors.DataSource = gr.LegendColors;
 
             }
+        }
+
+        private void GraphTitleBox_Click(object sender, EventArgs e)
+        {
+            GraphTitleBox.Text = "";
+            checkDrawTitle.Checked = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            // OK Button Click
         }
 
     }
